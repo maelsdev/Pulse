0fd43a3680fb7dba8993509743d7672df6ae90ac
Taras <maels@ukr.net>
2022-07-18 22:27:33 +0300

    carousel finish

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,22 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick();
+$(document).ready(function () {
+  $(".carousel__inner").slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: "linear",
+      prevArrow: '<button type="button" class="slick-prev"><img src="img/carousel/chevron-left-solid.png" alt="left-arrow"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="img/carousel/chevron-right-solid.png" alt="right-arrow"></button>',
+      responsive: [
+          {
+              breakpoint: 992,
+              settings: {
+                  arrows: false,
+                  dots:false,
+                
+              }
+          }
+      ]
   });
-          
+});
+
