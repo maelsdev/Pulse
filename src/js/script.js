827f7d48de15266fdc3a5d45e0658af7b89f1311
Taras <maels@ukr.net>
2022-07-22 12:34:48 +0300

    tabs finish

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+
   $(".carousel__inner").slick({
     dots: false,
     infinite: true,
@@ -18,5 +19,28 @@ $(document).ready(function () {
           }
       ]
   });
+
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab__active)', function() {
+    $(this)
+    .addClass('catalog__tab__active').siblings().removeClass('catalog__tab__active')
+    .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+
+ 
+
+  function toggleClass(item) { 
+    $(item).each(function (i) { 
+      $(this).on('click', function (e) { 
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content__active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list__active');
+      })
+    })
+  }
+
+  toggleClass('.catalog-item__link');
+  toggleClass('.catalog-item__back');
 });
+
+
 
